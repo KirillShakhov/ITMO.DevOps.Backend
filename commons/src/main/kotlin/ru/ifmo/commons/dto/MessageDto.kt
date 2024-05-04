1e3c7f2690ff600991d28d6c9bf4e46d43f8f444
KirillShakhov <kirill.shakhov14@gmail.com>
2024-05-04 17:05:48 +0300

    fix: rename mediafiles -> filesfiles

--- commons/src/main/kotlin/ru/ifmo/commons/dto/MessageDto.kt
+++ commons/src/main/kotlin/ru/ifmo/commons/dto/MessageDto.kt
@@ -3,10 +3,10 @@ package ru.ifmo.commons.dto
 import java.util.*
 
 data class MessageDto(
-    var id: String?,
-    var username: String,
-    var recipient: String,
-    var text: String,
-    var attachment: Int?,
-    var createdDate: Date?,
+    var id: String? = "",
+    var username: String = "",
+    var recipient: String = "",
+    var text: String = "",
+    var attachment: Int? = null,
+    var createdDate: Date? = null,
 )
