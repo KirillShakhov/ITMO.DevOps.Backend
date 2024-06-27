d857c9ff3b385111d0c545e92a124d4fd98e9779
Кирилл Шахов <kirill.shakhov14@gmail.com>
2024-06-28 02:40:38 +0300

    fix: message get by id test

--- chat/src/main/kotlin/ru/ifmo/chat/service/MessageService.kt
+++ chat/src/main/kotlin/ru/ifmo/chat/service/MessageService.kt
@@ -18,8 +18,4 @@ class MessageService @Autowired constructor(
         val message = Message(null, username, recipient, text, attachment, Date());
         return messageRepository.save(message);
     }
-
-    fun findById(id: String): Optional<Message> {
-        return messageRepository.findById(id);
-    }
 }
